4ec2e565a0dc1a632582f3772ba3fe4de80b1ddc
Gregory Moon <gregory.r.moon@gmail.com>
2015-01-08 20:13:46 -0700

    Initial Commit

---
+++ DTAutoDirectory/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace DTAutoDirectory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.createDirectoryButton = new System.Windows.Forms.Button();
+            this.useCaseSelectionBox = new System.Windows.Forms.ComboBox();
+            this.projectNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.responseLabel = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.issueNumberBox = new System.Windows.Forms.TextBox();
+            this.issueLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // createDirectoryButton
+            // 
+            this.createDirectoryButton.Location = new System.Drawing.Point(247, 95);
+            this.createDirectoryButton.Name = "createDirectoryButton";
+            this.createDirectoryButton.Size = new System.Drawing.Size(297, 49);
+            this.createDirectoryButton.TabIndex = 0;
+            this.createDirectoryButton.Text = "Create Project";
+            this.createDirectoryButton.UseVisualStyleBackColor = true;
+            this.createDirectoryButton.Click += new System.EventHandler(this.createDirectoryButton_Click);
+            // 
+            // useCaseSelectionBox
+            // 
+            this.useCaseSelectionBox.FormattingEnabled = true;
+            this.useCaseSelectionBox.Items.AddRange(new object[] {
+            "Use Case 1: Longitudinal statistical data",
+            "Use Case 2: KML/Shape of evolving geographic extents",
+            "Use Case 3: Inundations",
+            "Use Case 4: Time coded cumulative feature sets",
+            "Use Case 5: Historic track data",
+            "Use Case 10: Eras"});
+            this.useCaseSelectionBox.Location = new System.Drawing.Point(318, 12);
+            this.useCaseSelectionBox.Name = "useCaseSelectionBox";
+            this.useCaseSelectionBox.Size = new System.Drawing.Size(391, 24);
+            this.useCaseSelectionBox.TabIndex = 2;
+            this.useCaseSelectionBox.Text = "Choose a Use Case";
+            this.useCaseSelectionBox.SelectedIndexChanged += new System.EventHandler(this.useCaseSelectionBox_SelectedIndexChanged);
+            // 
+            // projectNameBox
+            // 
+            this.projectNameBox.Location = new System.Drawing.Point(153, 12);
+            this.projectNameBox.Name = "projectNameBox";
+            this.projectNameBox.Size = new System.Drawing.Size(159, 22);
+            this.projectNameBox.TabIndex = 3;
+            this.projectNameBox.TextChanged += new System.EventHandler(this.projectNameBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Project Name:";
+            // 
+            // responseLabel
+            // 
+            this.responseLabel.AutoSize = true;
+            this.responseLabel.Location = new System.Drawing.Point(663, 37);
+            this.responseLabel.Name = "responseLabel";
+            this.responseLabel.Size = new System.Drawing.Size(46, 17);
+            this.responseLabel.TabIndex = 5;
+            this.responseLabel.Text = "label2";
+            this.responseLabel.Visible = false;
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(472, 72);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(72, 17);
+            this.linkLabel.TabIndex = 6;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "linkLabel1";
+            this.linkLabel.Visible = false;
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Issue Number:";
+            // 
+            // issueNumberBox
+            // 
+            this.issueNumberBox.Location = new System.Drawing.Point(153, 40);
+            this.issueNumberBox.Name = "issueNumberBox";
+            this.issueNumberBox.Size = new System.Drawing.Size(100, 22);
+            this.issueNumberBox.TabIndex = 8;
+            this.issueNumberBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // issueLinkLabel
+            // 
+            this.issueLinkLabel.AutoSize = true;
+            this.issueLinkLabel.Location = new System.Drawing.Point(12, 72);
+            this.issueLinkLabel.Name = "issueLinkLabel";
+            this.issueLinkLabel.Size = new System.Drawing.Size(72, 17);
+            this.issueLinkLabel.TabIndex = 9;
+            this.issueLinkLabel.TabStop = true;
+            this.issueLinkLabel.Text = "linkLabel1";
+            this.issueLinkLabel.Visible = false;
+            this.issueLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.issueLinkLabel_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 156);
+            this.Controls.Add(this.issueLinkLabel);
+            this.Controls.Add(this.issueNumberBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.responseLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.projectNameBox);
+            this.Controls.Add(this.useCaseSelectionBox);
+            this.Controls.Add(this.createDirectoryButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "DTAutoDirectory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createDirectoryButton;
+        private System.Windows.Forms.ComboBox useCaseSelectionBox;
+        private System.Windows.Forms.TextBox projectNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label responseLabel;
+        private System.Windows.Forms.LinkLabel linkLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox issueNumberBox;
+        private System.Windows.Forms.LinkLabel issueLinkLabel;
+
+    }
+}
+
---
+++ DTAutoDirectory/MainForm.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace DTAutoDirectory
+{
+    public partial class MainForm : Form
+    {
+        private Dictionary<int, string> linkDict;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            linkDict = new Dictionary<int, string>()
+            {
+                {1, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_1_methods"},
+                {2, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_2_methods"},
+                {3, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_3_methods"},
+                {4, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_4_methods"},
+                {5, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_5_methods"},
+                {10, "https://dev.csf.asu.edu:1000/projects/mapstory/wiki/Use_case_10_(Eras)_methods"}
+            };
+        }
+
+        private void createDirectoryButton_Click(object sender, EventArgs e)
+        {
+            string driveName;
+
+            if (inputsGood())
+            {
+                if ((driveName = checkConnection()) != String.Empty)
+                {
+                    string dirLoc = getDirectoryLoc(), dirToAddTo = driveName + dirLoc + "\\";
+
+                    if (Directory.Exists(dirToAddTo))
+                        checkCreateFiles(dirToAddTo);
+                    else
+                    {
+                        dirToAddTo = driveName + "Projects\\MapStory\\" + dirLoc + "\\";
+
+                        if (Directory.Exists(dirToAddTo))
+                            checkCreateFiles(dirToAddTo);
+                        else
+                            MessageBox.Show("Directory: " + dirToAddTo + " was not found.", "Directory Not Found");
+                    }
+                }
+            }
+        }   //end createDirectoryButton_Click
+
+        private bool inputsGood()
+        {
+            string messageBoxText = String.Empty;
+            bool errFound = false;
+
+            this.projectNameBox.BackColor = Color.White;
+            this.useCaseSelectionBox.BackColor = Color.White;
+
+            if (String.IsNullOrEmpty(this.projectNameBox.Text))
+            {
+                this.projectNameBox.BackColor = Color.Red;
+                messageBoxText = "Enter a project name.";
+                errFound = true;
+            }
+
+            if (this.useCaseSelectionBox.SelectedItem == null)
+            {
+                if (errFound)
+                    messageBoxText = "Check your inputs.";
+                else
+                {
+                    errFound = true;
+                    messageBoxText = "Select a Use Case.";
+                }
+                this.useCaseSelectionBox.BackColor = Color.Red;
+            }
+
+            if (!validIssueNum())
+            {
+                if (errFound)
+                    messageBoxText = "Check your inputs.";
+                else
+                {
+                    errFound = true;
+                    messageBoxText = "Enter an issue number.";
+                }
+            }
+
+            if (errFound)
+                MessageBox.Show(messageBoxText, "Input Error");
+
+            return !errFound;
+        }
+
+        private string checkConnection()
+        {
+            DateTime now = System.DateTime.Now;
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.VolumeLabel == "dtpub$")
+                {
+                    TimeSpan test = System.DateTime.Now - now;
+                    double secs = test.TotalSeconds;
+                    responseLabel.Text = secs.ToString();
+                    return drive.Name;
+                }
+            }
+
+            MessageBox.Show("Not connected to \\\\bijou.dt.asu.edu\\dtpub$\\projects\\Mapstory.\nPlease connect and try again.", "Not Connected");
+
+            
+            return String.Empty;
+        }   //end checkConnection
+
+        private string getDirectoryLoc()
+        {
+            return "Use Case " + getSelectedUseCase().ToString();
+        }
+
+        private void checkCreateFiles(string loc)
+        {
+            string projName = this.projectNameBox.Text.Replace(' ', '_');
+            string projLoc = loc + issueNumberBox.Text + "_" + projName + "\\";
+
+            if (!Directory.Exists(projLoc))
+            {
+                createfiles(projLoc, projName);
+                showSuccessBox(projLoc, projName);
+                //resetInputs();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Project: " + projName + " already exists.\nWould you like to overwrite the existing project?", "Project Already Exists", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    result = MessageBox.Show("Are you sure you want to delete the existing project: " + projName + "?", "Are You Sure?", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        Directory.Delete(projLoc, true);
+                        createfiles(projLoc, projName);
+                        showSuccessBox(projLoc, projName);
+                    }
+                    else
+                        this.projectNameBox.BackColor = Color.Red;
+                }
+                else
+                    this.projectNameBox.BackColor = Color.Red;
+
+            }
+        }
+        private Int32 getSelectedUseCase()
+        {
+            return Int32.Parse(Regex.Match((string)this.useCaseSelectionBox.SelectedItem, @"\d+").Value);
+        }
+
+        private void createfiles(string projLoc, string projName)
+        {
+            DateTime now = System.DateTime.Now;
+            Directory.CreateDirectory(projLoc + projName + "_DATA");
+            Directory.CreateDirectory(projLoc + projName + "_DOC");
+            Directory.CreateDirectory(projLoc + projName + "_SHP_SLD");
+
+            File.WriteAllBytes(projLoc + projName + "_Map.mxd", Properties.Resources.blank_map);
+            File.WriteAllBytes(projLoc + projName + "_Database.mdb", Properties.Resources.blank_database);
+            File.Create(projLoc + projName + "_METADATA.txt").Close();
+            TimeSpan span = DateTime.Now - now;
+            double test = span.TotalSeconds;
+            this.responseLabel.Text = test.ToString();
+        }
+
+        private void resetInputs()
+        {
+            this.projectNameBox.Text = String.Empty;
+            this.projectNameBox.BackColor = Color.White;
+
+            this.useCaseSelectionBox.BackColor = Color.White;
+        }
+
+        private void showSuccessBox(string projLoc, string projName)
+        {
+            DialogResult result = MessageBox.Show("Successfully created project: " + projName + ".\nWould you like to open your new project?", "Success!", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+                Process.Start(projLoc);
+
+            projectNameBox.Text = String.Empty;
+        }
+
+        private void useCaseSelectionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            linkLabel.Text = "Use Case " + getSelectedUseCase() +  " methods";
+
+            if (!linkLabel.Visible) {
+                if (useCaseSelectionBox.BackColor == Color.Red)
+                    useCaseSelectionBox.BackColor = Color.White;
+
+                linkLabel.Visible = true;
+            }
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(linkDict[getSelectedUseCase()]);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(issueNumberBox.Text))
+            {
+                if (validIssueNum())
+                {
+                    if (!issueLinkLabel.Visible)
+                        issueLinkLabel.Visible = true;
+
+                    issueLinkLabel.Text = "Go to Issue #" + issueNumberBox.Text;
+                }
+                else
+                {
+                    if (issueLinkLabel.Visible)
+                        issueLinkLabel.Visible = false;
+                }
+            }
+            else
+            {
+                if (issueLinkLabel.Visible)
+                    issueLinkLabel.Visible = false;
+            }
+        }
+
+        private bool validIssueNum()
+        {
+            try
+            {
+                Int32.Parse(issueNumberBox.Text);
+
+                if (issueNumberBox.BackColor == Color.Red)
+                    issueNumberBox.BackColor = Color.White;
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (e is OverflowException || e is FormatException)
+                {
+                    Color test = issueNumberBox.BackColor;
+
+                    issueNumberBox.BackColor = Color.Red;
+
+                    return false;
+                }
+                else
+                {
+                    Debug.WriteLine(e.StackTrace);
+                    return false;
+                }
+            }
+        }
+
+        private void issueLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (validIssueNum())
+                Process.Start("https://dev.csf.asu.edu:1000/issues/" + Int32.Parse(issueNumberBox.Text));
+        }
+
+        private void projectNameBox_TextChanged(object sender, EventArgs e)
+        {
+            if (projectNameBox.BackColor == Color.Red)
+                projectNameBox.BackColor = Color.White;
+        }
+    }   //end MainForm
+}
